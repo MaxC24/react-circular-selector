3e007f28ca9ff66de4c488efc975482a0893e58d
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2018-09-10 12:24:23 +0200

    the fan rotates in the correct spot on click

--- src/index.js
+++ src/index.js
@@ -19,23 +19,33 @@ class Rotator extends React.Component {
         this.createRotationStyles();
     }
 
-    //TODO create a custome the currentAngle. so it adds the angle so rotates clockwise
     createRotationStyles(i) {
         //Creating the angle of each rotation.
         let numOfObjects = this.props.objects.length;
         let angle = 360/numOfObjects;
-        let rotationStyles = {};
-        let currentAngle = 180;
+        let rotationStyles = {...this.state.rotationStyles};
+        let currentAngle = rotationStyles[i] ? 
+            this._calculateCurrentAngle(rotationStyles[i].currentAngle) : 180;
+        let angleDiff = rotationStyles[i] ? currentAngle - rotationStyles[i].currentAngle : angle;
         let currentObject = this.state.selected;
-        for(let i = 0; i < numOfObjects; i++) {
+        for(let j = 0; j < numOfObjects; j++) {
             rotationStyles[currentObject] = {
                 transform: `rotate(${currentAngle}DEG)`,
-                transition: `transform 2s`
+                transition: `transform .6s`,
+                currentAngle: currentAngle
             }
-            currentAngle+=angle;
             currentObject = currentObject+1 < numOfObjects ? currentObject+1 : 0;
+            currentAngle = rotationStyles[currentObject] ? rotationStyles[currentObject].currentAngle : currentAngle;
+            currentAngle+=angleDiff;
         }
         this.setState({rotationStyles});
+    }
+    
+    _calculateCurrentAngle(angle) {
+        while((angle+180)%360!==0) {
+            angle+=1;
+        }
+        return angle
     }
     
     onClick(i) {
@@ -50,17 +60,17 @@ class Rotator extends React.Component {
         let { objects } = this.props;
         let { rotationStyles } = this.state;
         return(
-            <React.Fragment>
+            <div className="rotator__container">
                 {
                     objects.map((obj, i) => {
                         return (
-                            <div className="block" key={i} style={rotationStyles[i]} onClick={() => { this.onClick(i) }}>
+                            <div className="block" key={obj} style={rotationStyles[i]} onClick={() => { this.onClick(i) }}>
                                 {obj}
                             </div>
                         )
                     })
                 }
-            </React.Fragment>
+            </div>
         )
     }
 }
